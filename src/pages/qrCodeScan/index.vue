1b4c087720cf8d1112c279a9c31fcb5fd001255c
hieutst1997 <hieutst1997@gmail.com>
2023-11-04 15:35:42 +0700

    update

--- src/pages/qrCodeScan/index.vue
+++ src/pages/qrCodeScan/index.vue
@@ -1,28 +1,40 @@
 <template>
 	<div>
-		<div v-if="cameraReady">
+		<div v-if="cameraReady" class="relative">
 			<StreamBarcodeReader
 				@decode="onDecode"
 				@loaded="onLoaded"
+				class="relative"
 			></StreamBarcodeReader>
+			<div v-if="!isStreamReady" class="pending-qr">
+				<Loading />
+			</div>
 			<div>Scanned QR Code: {{ scannedData }}</div>
 		</div>
-		<div v-else>please allow camera to your device</div>
+		<div v-else class="pending-qr">
+			<img :src="icQrScan" alt="sadasd" class="w-60 h-auto" />
+
+			<p class="pt-3 text-sm">Please allow camera devices to scan</p>
+		</div>
 	</div>
 </template>
 
 <script>
 import { defineComponent, ref, onMounted } from "vue";
 import { StreamBarcodeReader } from "vue-barcode-reader";
+import icQrScan from "@/assets/images/ic_qr_scan.svg";
+import Loading from "@/components/Loading/loadingStyleOne.vue";
 
 export default defineComponent({
 	name: "QrCodeScan",
 	components: {
 		StreamBarcodeReader,
+		Loading,
 	},
 	setup() {
 		const scannedData = ref(null);
 		const cameraReady = ref(false);
+		const isStreamReady = ref(false);
 
 		function onDecode(text) {
 			console.log(`Decode text from QR code is ${text}`);
@@ -31,8 +43,8 @@ export default defineComponent({
 
 		function onLoaded() {
 			console.log(`Ready to start scanning barcodes`);
+			isStreamReady.value = true;
 		}
-
 
 		onMounted(async () => {
 			try {
@@ -51,6 +63,8 @@ export default defineComponent({
 			onLoaded,
 			scannedData,
 			cameraReady,
+			icQrScan,
+			isStreamReady,
 		};
 	},
 });
@@ -58,5 +72,17 @@ export default defineComponent({
 <style lang="scss" scoped>
 .scanner-container video {
 	width: 100% !important;
+}
+
+.pending-qr {
+	width: 100%;
+	height: calc(100vh - 76px);
+	overflow-y: auto;
+	background-color: #fcfcfc;
+	padding-bottom: 40px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 }
 </style>
